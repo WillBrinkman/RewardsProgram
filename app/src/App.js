74737c3637e20cad312518ca7740a6625c0edd22
William Brinkman <will.a.brinkman@gmail.com>
2021-06-05 12:15:48 -0500

    new grouping function

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react';
-import './App.css';
-import {Table} from 'reactstrap';
-
+import React, { Component } from "react";
+import "./App.css";
+import { Table } from "reactstrap";
 
 class App extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = {purchases : []}
+  constructor(props) {
+    super(props);
+    this.state = { purchases: [] };
   }
- 
- async componentDidMount(){
-  const response = await fetch('/api/purchases')
-  const body = await response.json();
-  this.setState({purchases : body})
-  //let {newPurchases} = purchases.map(v => Object.assign(v, { cost: 0}))
 
- // console.log(body);
-}
+  async componentDidMount() {
+    const response = await fetch("/api/purchases");
+    const body = await response.json();
+    this.setState({ purchases: body });
+    //let {newPurchases} = purchases.map(v => Object.assign(v, { cost: 0}))
 
+    // console.log(body);
+    console.log("compondntDidMount");
+  }
 
+  render() {
+    const { purchases } = this.state;
+    let Purchases = purchases.map((purchase) =>
+      Object.assign(purchase, { points: pointsCalculator(purchase) })
+    );
+    Purchases = groupBy(purchases, purchases.customerid);
+    //console.log(groupedPurchases);
 
-render() {
-  const {purchases} = this.state
-  let Purchases = purchases.map(purchase => Object.assign(purchase, { points: pointsCalculator(purchase)}))
- Purchases = groupBy(purchases, purchases.customerid)
-  //console.log(groupedPurchases);
+    function pointsCalculator(purchase) {
+      let roundedPurchaseCost = Math.floor(purchase.cost);
+      let purchasePoints = 0;
 
-      function pointsCalculator(purchase) {
-        let roundedPurchaseCost = (Math.floor(purchase.cost));
-        let purchasePoints = 0;
-
-        if (roundedPurchaseCost > 50){
-          purchasePoints = roundedPurchaseCost - 100 > 0 ? 50 + (roundedPurchaseCost-100)*2 :  roundedPurchaseCost - 50
-          return purchasePoints
+      if (roundedPurchaseCost > 50) {
+        purchasePoints =
+          roundedPurchaseCost - 100 > 0
+            ? 50 + (roundedPurchaseCost - 100) * 2
+            : roundedPurchaseCost - 50;
+        return purchasePoints;
       }
       return purchasePoints;
     }
@@ -42,35 +44,34 @@ render() {
       return Arr.reduce((newArr, obj) => {
         const key = obj[property];
         if (!newArr[key]) {
-           newArr[key] = [];
+          newArr[key] = [];
         }
         newArr[key].push(obj);
         return newArr;
-     }, {});
-  }
+      }, {});
+    }
 
+    // let Purchases = purchases.map( purchase => pointsCalculator(purchase))
 
- // let Purchases = purchases.map( purchase => pointsCalculator(purchase))
-  
- //Purchases.forEach(purchase => {v.points = pointsCalculator(purchase);}) ???
+    //Purchases.forEach(purchase => {v.points = pointsCalculator(purchase);}) ???
 
-//   let cost = (Math.floor(purchase.cost))
-//   if ((Math.floor(purchase.cost)) > 50){
-//     purchase.points = cost - 100 > 0 ? 50 + (cost-100)*2 :  cost - 50
-// }
+    //   let cost = (Math.floor(purchase.cost))
+    //   if ((Math.floor(purchase.cost)) > 50){
+    //     purchase.points = cost - 100 > 0 ? 50 + (cost-100)*2 :  cost - 50
+    // }
+    //
 
-  let rows = 
-  Purchases.map( purchase => 
+    let rows = Purchases.map((purchase) => (
+      <tr key={purchase.id}>
+        <td>{purchase.customer}</td>
+        <td>${purchase.cost}</td>
+        <td>{purchase.date}</td>
+        <td>{purchase.description}</td>
+        <td>{purchase.points}</td>
+      </tr>
+    ));
 
-    <tr key = {purchase.id}>
-      <td>{purchase.customer}</td>
-      <td>${purchase.cost}</td>
-      <td>{purchase.date}</td>
-      <td>{purchase.description}</td>
-      <td>{purchase.points}</td>
-    </tr>)
-
-    return(
+    return (
       <div>
         <h2> Customer Points Tracker</h2>
         <Table>
@@ -83,16 +84,10 @@ render() {
               <th>Points</th>
             </tr>
           </thead>
-          <tbody>
-            {rows}
-          </tbody>
-
+          <tbody>{rows}</tbody>
         </Table>
       </div>
-    )
-    }
-
-
-
+    );
+  }
 }
 export default App;
